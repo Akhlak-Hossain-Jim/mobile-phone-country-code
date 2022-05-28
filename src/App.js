fac36e2ff163ae7c49f7d5c32828095745913474
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2022-05-29 00:20:55 +0600

    🛠️completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BiSearchAlt } from "react-icons/bi";
+import AppContainer from "./style";
+import data from "./phoneCode.json";
 
-function App() {
+export default function App() {
+  const [Search, setSearch] = React.useState("");
+  const [Data, setData] = React.useState([...data]);
+  console.table(Data);
+
+  React.useEffect(() => {
+    Search &&
+      setData(
+        Search.match(/^[a-zA-Z]+$/)
+          ? data.filter((d) => {
+              return d.country.toLowerCase().includes(Search.toLowerCase());
+            })
+          : data.filter((d) => {
+              return d.code.toString().includes(Search.toString());
+            })
+      );
+  }, [Search, Data]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+    <AppContainer>
+      <header>
+        <div className="search__box">
+          <input
+            type="text"
+            onChange={(e) => setSearch(e.target.value)}
+            placeholder="Search by Country Name/Code (without the '+' sign)"
+          />
+          <BiSearchAlt />
+        </div>
+      </header>
+      {Data.length !== 0 ? (
+        <main>
+          <div className="title">Country</div>
+          <div className="title">Code</div>
+          {React.Children.toArray(
+            Data.map((d) => (
+              <>
+                <div>{d.country}</div>
+                <div>+{d.code}</div>
+              </>
+            ))
+          )}
+        </main>
+      ) : (
+        <main className="error">
+          <img src="/empty.svg" alt="empty illustration" />
+          <h1>
+            Sorry no "Country Code" with the Country{" "}
+            {Search.match(/^[a-zA-Z]+$/) ? "Name" : "Code"} of "+{Search}"
+            found!
+          </h1>
+        </main>
+      )}
+      <footer>
+        by{" "}
+        <a href="http://ahjim.com" target="_blank" rel="noopener noreferrer">
+          Akhlak Hossain Jim
+        </a>
+        . Find the code{" "}
         <a
-          className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/Akhlak-Hossain-Jim/mobile-phone-country-code"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          here.
         </a>
-      </header>
-    </div>
+      </footer>
+    </AppContainer>
   );
 }
-
-export default App;
